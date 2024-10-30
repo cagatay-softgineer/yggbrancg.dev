cb81aa11d5ae644f7d60dc599ec41eedbdb81976
Ali Bulut <52183607+cagatay-softgineer@users.noreply.github.com>
2024-10-30 17:43:36 +0300

    Change All Project

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Feedback(Base):
-    __tablename__ = "feedback"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    message = Column(String)
--- run.bat
+++
@@ -1,3 +0,0 @@
-echo python -m uvicorn app.main:app --port 8000
-uvicorn app.main:app --host 0.0.0.0 --port 8080
-pause
